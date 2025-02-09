e217fade51b1c52f5e8f17a02c0b36cc5e3c60ee
MalTeeez <41554385+MalTeeez@users.noreply.github.com>
2025-02-09 04:58:17 +0100

    Patch database folder to be better compatible with mounts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/sh
 mkdir -p /usr/local/src/moe-counter
 cd /usr/local/src/moe-counter
-
+# Init repo, then fill from remote
 git init
 git remote add origin https://github.com/journey-ad/Moe-Counter.git
 git pull
 git checkout master -f
 git branch --set-upstream-to origin/master
-
+# Fix database location to work with mounts
+mkdir -p /usr/local/src/moe-counter/data/
+sed -i "s|../count.db|../data/count.db|g" /usr/local/src/moe-counter/db/sqlite.js
+# Install npm packages and start webserver
 npm install
 npm run start
