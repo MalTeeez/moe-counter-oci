3cc3e45a03ab13d6190786208d703563c62015bd
MalTeeez <41554385+MalTeeez@users.noreply.github.com>
2025-02-09 19:34:28 +0100

    Switch node runtime to bun, should be better performance overall

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-FROM node:20-alpine3.21
+FROM oven/bun:1.2.2-alpine
 
 COPY entrypoint.sh /
 
-RUN chmod +x entrypoint.sh && apk add git
+# Enable entrypoint & Install git
+RUN chmod +x /entrypoint.sh && apk add git
+# Limit git to low memory
 RUN git config --global pack.windowMemory "50m" \
     && git config --global pack.packSizeLimit "50m" \
     && git config --global pack.threads "1"
 
+# Config
 EXPOSE 3000
+ENV LOG_LEVEL='warn'
+
 
 WORKDIR /usr/local/src/moe-counter
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,14 @@ git remote add origin https://github.com/journey-ad/Moe-Counter.git
 git pull
 git checkout master -f
 git branch --set-upstream-to origin/master
-# Fix database location to work with mounts
+# Fix database location to work with mounts & use buns faster sqlite driver
 mkdir -p /usr/local/src/moe-counter/data/
 sed -i "s|../count.db|../data/count.db|g" /usr/local/src/moe-counter/db/sqlite.js
+sed -i "s|('better-sqlite3')|('bun:sqlite').Database|g" /usr/local/src/moe-counter/db/sqlite.js
+# Always log the start message
+sed -i "s|logger.info(\"Your app|console.log(\"Your app|g" /usr/local/src/moe-counter/index.js
+# And remove old sqlite driver
+bun remove better-sqlite3
 # Install npm packages and start webserver
-npm install
-npm run start
+bun install
+bun run ./index.js
