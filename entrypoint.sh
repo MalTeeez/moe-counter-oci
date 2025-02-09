2d35f968ca9cd4ca17cd0b98685456ae1f7f1b1f
MalTeeez <41554385+MalTeeez@users.noreply.github.com>
2025-02-09 04:47:07 +0100

    Don't let git get blocked by a non-empty folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/sh
-git clone https://github.com/journey-ad/Moe-Counter.git /usr/local/src/moe-counter \
-    && cd /usr/local/src/moe-counter \
-    && npm install \
-    && npm run start
+mkdir -p /usr/local/src/moe-counter
+cd /usr/local/src/moe-counter
+
+git init
+git remote add origin https://github.com/journey-ad/Moe-Counter.git
+git pull
+git checkout master -f
+git branch --set-upstream-to origin/master
+
+npm install
+npm run start
